fb2faf46bf2fb9b7e7ee4b1a73418cd326f59224
Vladislav <Dorofeevvlad56@bk.ru>
2023-08-22 20:03:31 +1000

    init

--- PycharmProjects/pythonProject1/lesson 4/advertisment/app_advertisment/urls.py
+++ PycharmProjects/pythonProject1/lesson 4/advertisment/app_advertisment/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, advertisement, advertisement_post,register,login,profile,oktotorp
 
 urlpatterns = [
-    path('', index)
+    path('', index, name= "main-page"),
+    path('topsellers',top_sellers,name="top-sellers"),
+    path('advertisement',advertisement,name="advertisement"),
+    path('advertisement-post',advertisement_post,name="advertisement-post"),
+    path('register',register , name="register"),
+    path('login',login , name="login"),
+    path('profile',profile , name="profile"),
+    path('oktotorp',oktotorp,name="oktotorp"),
+
 ]
 
--- PycharmProjects/pythonProject1/lesson 4/advertisment/app_advertisment/views.py
+++ PycharmProjects/pythonProject1/lesson 4/advertisment/app_advertisment/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Успешно!")
+    return render(request,'index.html')
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+def advertisement(request):
+    return render(request, 'advertisement.html')
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
+def register(request):
+    return render(request, 'register.html')
+def login(request):
+    return render(request,'login.html')
+def profile(request):
+    return render(request,'profile.html')
+def oktotorp(request):
+    return render(request,"#.html")
+
